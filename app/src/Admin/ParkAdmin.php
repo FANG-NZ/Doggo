665c3be7c3913b2a0bbc72cff2e64a91ed847b43
Fang (MacPro) <faang1980@gmail.com>
2021-03-27 20:41:06 +1300

    setup new layout for ParkAdmin

--- app/src/Admin/ParkAdmin.php
+++ app/src/Admin/ParkAdmin.php
@@ -7,16 +7,68 @@ use Doggo\Model\Park;
 use SilverStripe\Forms\GridField\GridField;
 use SilverStripe\Forms\GridField\GridFieldDetailForm;
 use Doggo\Admin\Extensions\ParkGridFieldDetailForm_ItemRequest;
+use SilverStripe\Forms\GridField\GridFieldExportButton;
+use SilverStripe\Forms\GridField\GridFieldImportButton;
+use SilverStripe\Forms\GridField\GridFieldPrintButton;
+use SilverStripe\Forms\GridField\GridFieldAddNewButton;
 
 class ParkAdmin extends ModelAdmin
 {
     private static $managed_models = [
-        Park::class,
+        Park::class => ['title' => "All parks"],
+        "Doggo-Model-Park-Pending" => ['title' => "Pending Images"],
     ];
 
     private static $menu_title = 'Parks';
 
     private static $url_segment = 'parks';
+
+    /**
+     * Define the vars to indicate if we need to show Pending Park List
+     */
+    private $showPendingList = false;
+
+    /**
+     * OVERRIDE
+     */
+    protected function init()
+    {
+        parent::init();
+
+        if($this->showPendingList){
+            $this->modelTab = "Doggo-Model-Park-Pending";
+        }
+    }
+
+    /**
+     * OVERRIDE
+     */
+    protected function unsanitiseClassName($class)
+    {
+        if($class === "Doggo-Model-Park-Pending"){
+            $class = "Doggo-Model-Park";
+            $this->showPendingList = true;
+        }
+        return parent::unsanitiseClassName($class);
+    }
+
+    /**
+     * OVERRIDE
+     */
+    public function getList()
+    {
+        $list = parent::getList();
+
+        //If showPendingList set TRUE, we need to filter
+        //item has PendingImage
+        if($this->showPendingList){
+            $list = $list->filterByCallback(function($item, $selflist){
+                return $item->hasPendingImage();
+            });
+        }
+
+        return $list;
+    }
 
 
     /**
@@ -28,9 +80,19 @@ class ParkAdmin extends ModelAdmin
 
         $config = $field->getConfig();
 
+        //To remove buttons form PendingList panel
+        if($this->showPendingList){
+            $config
+                ->removeComponentsByType(GridFieldAddNewButton::class)
+                ->removeComponentsByType(GridFieldExportButton::class)
+                ->removeComponentsByType(GridFieldImportButton::class)
+                ->removeComponentsByType(GridFieldPrintButton::class);
+        }
+
         $detailForm = $config->getComponentByType(GridFieldDetailForm::class);
         $detailForm->setItemRequestClass(ParkGridFieldDetailForm_ItemRequest::class);
 
         return $field;
     }
+
 }
